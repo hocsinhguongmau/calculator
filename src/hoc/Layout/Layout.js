b58ce2d6fd84038c7c23b645f72ed5b7b2d6e6e2
thang241190 <thangnguyen24111990@gmail.com>
2020-01-07 11:37:57 +0200

    Add gh pages

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import Display from "../../components/Display/Display";
 import Buttons from "../../components/Buttons/Buttons";
 
-const Layout = () => (
-	<div className="container">
-		<div className="columns is-centered is-gapless">
-			<div className="column is-half">
-				<h1 className="title">Calculator</h1>
-				<Display />
-				<Buttons />
+class Layout extends Component {
+	state = {
+		number: 0,
+		currentOperator: null,
+		formula: null
+	};
+
+	inputHandler=()=>{
+		
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="columns is-centered is-gapless">
+					<div className="column is-half">
+						<h1 className="title">Calculator</h1>
+						<Display />
+						<Buttons changeInput={this.inputHandler} />
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-);
+		);
+	}
+}
 
 export default Layout;
